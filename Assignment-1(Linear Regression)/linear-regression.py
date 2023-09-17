dc09467fcd656a2c1f705ea962a6a64cd1abb34f
anuz4rkh4n <you@example.com>
2023-09-18 00:06:07 +0500

    changes to model

--- Assignment-1(Linear Regression)/linear-regression.py
+++ Assignment-1(Linear Regression)/linear-regression.py
@@ -5,18 +5,27 @@ from sklearn import datasets
 
 boston = datasets.load_diabetes()
 
-X = boston.data
-y = boston.target
+# print(boston.keys())
+# print(boston.data)
+
+diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+
+diabetes_X = diabetes_X[:, np.newaxis, 2]
+
+X_train = diabetes_X[:-100]
+X_test = diabetes_X[-100:]
+
+y_train = diabetes_y[:-100]
+y_test = diabetes_y[-100:]
 
 model = LinearRegression()
 
-model.fit(X, y)
+model.fit(X_train, y_train)
 
-y_pred = model.predict(X)
+y_pred = model.predict(X_test)
 
-plt.scatter(y, y_pred)
-
-plt.plot(y, y, color='green', linestyle='dashed')
+plt.scatter(X_test, y_test, color="green")
+plt.plot(X_test, y_pred, color="red", linewidth=2)
 
 plt.xlabel('Actual values')
 plt.ylabel('Predicted values')
